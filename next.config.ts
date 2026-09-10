4d6bf1cf0b0ab39c23996841be9fe5091295f0c4
Haidar Bahzi <217346916+thehaidarbahzi@users.noreply.github.com>
2026-09-10 16:07:20 +0700

    (thehaidarbahzi): fixing next image allowance

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "187.77.114.147",
+        port: "9981",
+        pathname: "/statics/qrcode/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
