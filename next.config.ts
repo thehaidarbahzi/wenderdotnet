8a011a6f121bb57c8b5e48034a43c5d895c56c59
Haidar Bahzi <217346916+thehaidarbahzi@users.noreply.github.com>
2026-09-10 17:25:42 +0700

    idk ignore this siht

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "187.77.114.147",
-        port: "9981",
-        pathname: "/statics/qrcode/**",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
